c4cd34945b9c3f3d6f109ad3fe96837aba5a9c49
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-12-30 18:04:07 +0530

    Add project files.

---
+++ WebChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebChatApp.Models;
+
+namespace WebChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
---
+++ WebChatApp/wwwroot/js/signalrReqHandler.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+
+connection.on("ReceiveMessage", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("Messages").appendChild(li);
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("Send").addEventListener("click", function (event) {
+    var message = document.getElementById("Message").value;
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
